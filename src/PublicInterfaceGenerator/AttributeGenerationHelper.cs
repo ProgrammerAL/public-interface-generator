29b102da7544893b3a8bcb1d7a3bf981bdccb873
AL Rodriguez <ProgrammerAL@outlook.com>
2025-04-28 12:10:29 -0400

    More code styling changes for the generated interfaces

--- src/PublicInterfaceGenerator/AttributeGenerationHelper.cs
+++ src/PublicInterfaceGenerator/AttributeGenerationHelper.cs
@@ -12,12 +12,16 @@ public class AttributeGenerationHelper
         _ = builder.AppendLine($"#nullable enable");
         _ = builder.AppendLine(GenerateAttributionsSuppressComments());
 
+        //Another line to separate code from rules
+        _ = builder.AppendLine();
+
         _ = builder.AppendLine(GenerateEmbeddedAttribute());
         _ = builder.AppendLine();
-        _ = builder.AppendLine(GenerateExcludeFromGeneratedInterfaceAttribute());
-        _ = builder.AppendLine();
-        _ = builder.AppendLine(GenerateGenerateInterfaceAttribute());
+        _ = builder.AppendLine(GenerateInterfaceAttributes());
         
+        //Another line to separate code from rules
+        _ = builder.AppendLine();
+
         _ = builder.Append(GenerateAttributionsSuppressionRestoreComments());
 
         return builder.ToString();
@@ -41,63 +45,57 @@ public class AttributeGenerationHelper
             """;
     }
 
-    public static string GenerateExcludeFromGeneratedInterfaceAttribute()
+    public static string GenerateInterfaceAttributes()
     {
         return
             """
             namespace ProgrammerAl.SourceGenerators.PublicInterfaceGenerator.Attributes
             {
                 [System.AttributeUsage(System.AttributeTargets.Method | System.AttributeTargets.Property | System.AttributeTargets.Event, Inherited = false, AllowMultiple = false)]
-                [Microsoft.CodeAnalysis.EmbeddedAttribute, System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverageAttribute]
+                [Microsoft.CodeAnalysis.EmbeddedAttribute]
+                [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverageAttribute]
                 public sealed class ExcludeFromGeneratedInterfaceAttribute : System.Attribute
                 {
                 }
-            }
-            """;
-    }
 
-    public static string GenerateGenerateInterfaceAttribute()
-    {
-        return """
-            namespace ProgrammerAl.SourceGenerators.PublicInterfaceGenerator.Attributes
-            {
                 [System.AttributeUsage(System.AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
-                [Microsoft.CodeAnalysis.EmbeddedAttribute, System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverageAttribute]
+                [Microsoft.CodeAnalysis.EmbeddedAttribute]
+                [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverageAttribute]
                 public sealed class GenerateInterfaceAttribute : System.Attribute
                 {
                     /// <summary>
                     /// Set this to override the default interface name. Or leave it null to use the class name with an 'I' prepended to it.
                     /// </summary>
                     public string? InterfaceName { get; set; }
-
+            
                     /// <summary>
                     /// Set this to override the namespace to generate the interface in. By default, it will be the same as the class.
                     /// </summary>
                     public string? Namespace { get; set; }
-
+            
                     /// <summary>
-                    /// Set this to specify the interfaces the generated interface will inherit from. For example, IDisposable. 
+                    /// Set this to specify the interfaces the generated interface will inherit from. For example, IDisposable.
                     /// This should be a syntax-valid list as you would type it out normally because it will be concatenated directly into the interface definition.
                     /// For example: ""MyNamespace.MyInterface1, MyNamespace.MyInterface2""
                     /// </summary>
                     public string? Interfaces { get; set; }
-
+            
                     /// <summary>
                     /// Set this to specify the generated interface inherits from System.IDisposable.
                     /// This will be appended to the list of interfaces the generated interface inherits from.
                     /// This is in addition to the <see cref="Interfaces"/> property.
-                    /// If you are also specifying interfaces with the <see cref="Interfaces"/> property, 
-                    ///   either set this to false and include "System.IDisposable" in the <see cref="Interfaces"/> property string, 
+                    /// If you are also specifying interfaces with the <see cref="Interfaces"/> property,
+                    ///   either set this to false and include "System.IDisposable" in the <see cref="Interfaces"/> property string,
                     ///   or set this to true and don't include "System.IDisposable" in the <see cref="Interfaces"/> string.
                     ///   Failure to do this will result in System.IDisposable being appended to the generated interface twice.
                     /// </summary>
                     public bool IsIDisposable { get; set; }
-
+            
                     /// <summary>
                     /// Set this to specify the generated interface inherits from <see cref="System.IAsyncDisposable"/>.
                     /// This is in addition to the <see cref="Interfaces"/> property.
-                    /// If you are also specifying interfaces with the <see cref="Interfaces"/> property, 
-                    ///   either set this to false and include "System.IAsyncDisposable" in the <see cref="Interfaces"/> property string, 
+                    /// If you are also specifying interfaces with the <see cref="Interfaces"/> property,
+                    ///   either set this to false and include "System.IAsyncDisposable" in the <see cref="Interfaces"/> property string,
                     ///   or set this to true and don't include "System.IAsyncDisposable" in the <see cref="Interfaces"/> string.
                     ///   Failure to do this will result in System.IAsyncDisposable being appended to the generated interface twice.
                     /// </summary>
@@ -112,9 +110,9 @@ public class AttributeGenerationHelper
         return """
             #pragma warning disable SA1649 // SA1649FileNameMustMatchTypeName
             #pragma warning disable SA1402 // FileMayOnlyContainASingleType
+            #pragma warning disable SA1403 // SA1403FileMayOnlyContainASingleNamespace
             #pragma warning disable SA1502 // ElementMustNotBeOnSingleLine
-            #pragma warning disable SA1013 // ClosingBracesMustBeSpacedCorrectly
-                       
+            #pragma warning disable SA1013 // ClosingBracesMustBeSpacedCorrectly                       
             """;
     }
 
@@ -123,6 +121,7 @@ public class AttributeGenerationHelper
         return """
             #pragma warning restore SA1649 // SA1649FileNameMustMatchTypeName
             #pragma warning restore SA1402 // FileMayOnlyContainASingleType
+            #pragma warning restore SA1403 // SA1403FileMayOnlyContainASingleNamespace
             #pragma warning restore SA1502 // ElementMustNotBeOnSingleLine
             #pragma warning restore SA1013 // ClosingBracesMustBeSpacedCorrectly
             """;
--- src/UnitTests/Snapshots/Properties/PropertiesTests.PrivateSettersProperties_AssertResults#PublicInterfaceGeneratorAttributes.verified.cs
+++ src/UnitTests/Snapshots/Properties/PropertiesTests.PrivateSettersProperties_AssertResults#PublicInterfaceGeneratorAttributes.verified.cs
@@ -2,9 +2,10 @@
 #nullable enable
 #pragma warning disable SA1649 // SA1649FileNameMustMatchTypeName
 #pragma warning disable SA1402 // FileMayOnlyContainASingleType
+#pragma warning disable SA1403 // SA1403FileMayOnlyContainASingleNamespace
 #pragma warning disable SA1502 // ElementMustNotBeOnSingleLine
-#pragma warning disable SA1013 // ClosingBracesMustBeSpacedCorrectly
-           
+#pragma warning disable SA1013 // ClosingBracesMustBeSpacedCorrectly                       
+
 namespace Microsoft.CodeAnalysis
 {
     [System.AttributeUsage(System.AttributeTargets.All)]
@@ -15,16 +16,15 @@ namespace Microsoft.CodeAnalysis
 namespace ProgrammerAl.SourceGenerators.PublicInterfaceGenerator.Attributes
 {
     [System.AttributeUsage(System.AttributeTargets.Method | System.AttributeTargets.Property | System.AttributeTargets.Event, Inherited = false, AllowMultiple = false)]
-    [Microsoft.CodeAnalysis.EmbeddedAttribute, System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverageAttribute]
+    [Microsoft.CodeAnalysis.EmbeddedAttribute]
+    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverageAttribute]
     public sealed class ExcludeFromGeneratedInterfaceAttribute : System.Attribute
     {
     }
-}
 
-namespace ProgrammerAl.SourceGenerators.PublicInterfaceGenerator.Attributes
-{
     [System.AttributeUsage(System.AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
-    [Microsoft.CodeAnalysis.EmbeddedAttribute, System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverageAttribute]
+    [Microsoft.CodeAnalysis.EmbeddedAttribute]
+    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverageAttribute]
     public sealed class GenerateInterfaceAttribute : System.Attribute
     {
         /// <summary>
@@ -38,7 +38,7 @@ namespace ProgrammerAl.SourceGenerators.PublicInterfaceGenerator.Attributes
         public string? Namespace { get; set; }
 
         /// <summary>
-        /// Set this to specify the interfaces the generated interface will inherit from. For example, IDisposable. 
+        /// Set this to specify the interfaces the generated interface will inherit from. For example, IDisposable.
         /// This should be a syntax-valid list as you would type it out normally because it will be concatenated directly into the interface definition.
         /// For example: ""MyNamespace.MyInterface1, MyNamespace.MyInterface2""
         /// </summary>
@@ -48,8 +48,8 @@ namespace ProgrammerAl.SourceGenerators.PublicInterfaceGenerator.Attributes
         /// Set this to specify the generated interface inherits from System.IDisposable.
         /// This will be appended to the list of interfaces the generated interface inherits from.
         /// This is in addition to the <see cref="Interfaces"/> property.
-        /// If you are also specifying interfaces with the <see cref="Interfaces"/> property, 
-        ///   either set this to false and include "System.IDisposable" in the <see cref="Interfaces"/> property string, 
+        /// If you are also specifying interfaces with the <see cref="Interfaces"/> property,
+        ///   either set this to false and include "System.IDisposable" in the <see cref="Interfaces"/> property string,
         ///   or set this to true and don't include "System.IDisposable" in the <see cref="Interfaces"/> string.
         ///   Failure to do this will result in System.IDisposable being appended to the generated interface twice.
         /// </summary>
@@ -58,15 +58,17 @@ namespace ProgrammerAl.SourceGenerators.PublicInterfaceGenerator.Attributes
         /// <summary>
         /// Set this to specify the generated interface inherits from <see cref="System.IAsyncDisposable"/>.
         /// This is in addition to the <see cref="Interfaces"/> property.
-        /// If you are also specifying interfaces with the <see cref="Interfaces"/> property, 
-        ///   either set this to false and include "System.IAsyncDisposable" in the <see cref="Interfaces"/> property string, 
+        /// If you are also specifying interfaces with the <see cref="Interfaces"/> property,
+        ///   either set this to false and include "System.IAsyncDisposable" in the <see cref="Interfaces"/> property string,
         ///   or set this to true and don't include "System.IAsyncDisposable" in the <see cref="Interfaces"/> string.
         ///   Failure to do this will result in System.IAsyncDisposable being appended to the generated interface twice.
         /// </summary>
         public bool IsIAsyncDisposable { get; set; }
     }
 }
+
 #pragma warning restore SA1649 // SA1649FileNameMustMatchTypeName
 #pragma warning restore SA1402 // FileMayOnlyContainASingleType
+#pragma warning restore SA1403 // SA1403FileMayOnlyContainASingleNamespace
 #pragma warning restore SA1502 // ElementMustNotBeOnSingleLine
 #pragma warning restore SA1013 // ClosingBracesMustBeSpacedCorrectly
